fbf133ee11d1af9124258bbbead44036d5ca8e78
himomohi <Himomohi@gmail.com>
2023-12-20 15:57:22 +0900

    update

--- main_imgs.py
+++ main_imgs.py
@@ -3,6 +3,7 @@
 import streamlit as st
 
 import PIL.Image
+import time
 
 #API 키 파일을 불러오는 코드
 api_key = st.secrets['GOOGLE_API_KEY']
@@ -20,6 +21,9 @@ genai.configure(api_key=api_key)  # credentials 인자 제거
 
 
 st.header("SNS Posting AI")
+# 세션 상태 초기화
+if 'last_request_time' not in st.session_state:
+    st.session_state['last_request_time'] = 0
 
 
 #이미지 업로드
@@ -27,9 +31,18 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:       
        img = PIL.Image.open(uploaded_file)
        models= genai.GenerativeModel('gemini-pro-vision')
-       with st.spinner("😀SNS 포스팅 내용을 작성중이에요."):
-        response=models.generate_content([prompt,img])
-        with st.chat_message("ai"):
-            st.image(img)
-            st.write(response.text)
+        # 현재 시간 및 마지막 요청 시간 확인
+       current_time = time.time()
+       last_request_time = st.session_state['last_request_time']
+
+       # 1분 이내에 요청이 있었는지 확인
+       if current_time - last_request_time < 60:
+            st.warning("1분 안에 다시 요청할 수 없습니다.")
+       else:
+
+            with st.spinner("😀SNS 포스팅 내용을 작성중이에요."):
+                response=models.generate_content([prompt,img])
+                with st.chat_message("ai"):
+                    st.image(img)
+                    st.write(response.text)
 
